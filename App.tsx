bdb0a2a5ed57fe73e745e9bc322fbe28089ede59
dev6699 <kweijack.dev@gmail.com>
2022-07-24 14:36:51 +0800

    feat: added app icon, chg app name

--- App.tsx
+++ App.tsx
@@ -1,38 +1,11 @@
 import 'react-native/tvos-types.d';
-import React, { useEffect, useRef } from 'react';
-import {
-  SafeAreaView,
-  ActivityIndicator,
-  Animated,
-  Easing,
-  View,
-  Text,
-} from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, ActivityIndicator, View, Text } from 'react-native';
 
 import { useNav, NavContext, useVideo, VideoContext } from './src/hooks';
 import { Screen } from './src/screens';
 
 const App = () => {
-  const animation = useRef(new Animated.Value(0)).current;
-
-  const boxInterpolation = animation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['rgb(255,255,255)', 'rgb(0,0,0)'],
-  });
-
-  const handleAnimation = () => {
-    Animated.timing(animation, {
-      useNativeDriver: false,
-      toValue: 1,
-      duration: 2000,
-      easing: Easing.linear,
-    }).start();
-  };
-
-  useEffect(() => {
-    handleAnimation();
-  }, []);
-
   const navState = useNav();
   const videoState = useVideo(navState);
 
@@ -50,7 +23,7 @@ const App = () => {
   return (
     <VideoContext.Provider value={videoState}>
       <NavContext.Provider value={navState}>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
           {error ? (
             <View
               style={{
@@ -67,25 +40,18 @@ const App = () => {
               </Text>
             </View>
           ) : null}
-          <Animated.View
-            style={{
-              flex: 1,
-              padding: 10,
-              backgroundColor: boxInterpolation,
-            }}>
-            {videoState.state.init ? (
-              <Screen />
-            ) : (
-              <View
-                style={{
-                  flex: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <ActivityIndicator size={100} color="#4287f5" />
-              </View>
-            )}
-          </Animated.View>
+          {videoState.state.init ? (
+            <Screen />
+          ) : (
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <ActivityIndicator size={100} color="#4287f5" />
+            </View>
+          )}
         </SafeAreaView>
       </NavContext.Provider>
     </VideoContext.Provider>
