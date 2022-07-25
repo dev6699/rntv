b97df453235d7cc0b5354b0dbc1ce93b6f3da35d
dev6699 <kweijack.dev@gmail.com>
2022-07-25 09:09:22 +0800

    fix: mv touchable show controls to video area only

--- src/components/VideoPlayer/Video.tsx
+++ src/components/VideoPlayer/Video.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 //@ts-ignore
 import RNVideo from 'react-native-video';
 import { useVideoPlayerContext } from './useVideoPlayer';
@@ -7,25 +8,29 @@ export const Video: React.FC<{ uri: string }> = ({ uri }) => {
   const { state, refs, events } = useVideoPlayerContext();
 
   return (
-    <RNVideo
-      ref={refs.videoPlayerRef}
-      resizeMode={'contain'}
-      paused={state.paused}
-      rate={state.rate}
-      onLoadStart={events.onLoadStart}
-      onProgress={events.onProgress}
-      onError={events.onError}
-      onLoad={events.onLoad}
-      onEnd={() => {
-        // TODO
-        console.log('video ended, go to next or close');
-      }}
-      onSeek={events.onSeek}
-      style={{
-        height: '100%',
-        width: '100%',
-      }}
-      source={{ uri }}
-    />
+    <TouchableWithoutFeedback
+      hasTVPreferredFocus={false}
+      onPress={events.onScreenTouch}>
+      <RNVideo
+        ref={refs.videoPlayerRef}
+        resizeMode={'contain'}
+        paused={state.paused}
+        rate={state.rate}
+        onLoadStart={events.onLoadStart}
+        onProgress={events.onProgress}
+        onError={events.onError}
+        onLoad={events.onLoad}
+        onEnd={() => {
+          // TODO
+          console.log('video ended, go to next or close');
+        }}
+        onSeek={events.onSeek}
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+        source={{ uri }}
+      />
+    </TouchableWithoutFeedback>
   );
 };
--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableWithoutFeedback, View } from 'react-native';
+import { View } from 'react-native';
 
 import { VideoPlayerContext, useVideoPlayer } from './useVideoPlayer';
 
@@ -12,28 +12,22 @@ export const VideoPlayer: React.FC<{ uri: string }> = ({ uri }) => {
   const videoPlayerState = useVideoPlayer();
   const {
     state: { error, loading },
-    events: { onScreenTouch },
   } = videoPlayerState;
 
   return (
     <VideoPlayerContext.Provider value={videoPlayerState}>
-      <TouchableWithoutFeedback
-        hasTVPreferredFocus={false}
-        onPress={onScreenTouch}>
-        <View
-          style={{
-            overflow: 'hidden',
-            backgroundColor: '#000',
-            flex: 1,
-            alignSelf: 'stretch',
-            justifyContent: 'space-between',
-          }}>
-          <Video uri={uri} />
-          <ControlBar />
-          {error && <Error />}
-          {loading && <Loader />}
-        </View>
-      </TouchableWithoutFeedback>
+      <View
+        style={{
+          backgroundColor: '#000',
+          flex: 1,
+          alignSelf: 'stretch',
+          justifyContent: 'space-between',
+        }}>
+        <Video uri={uri} />
+        <ControlBar />
+        {error && <Error />}
+        {loading && <Loader />}
+      </View>
     </VideoPlayerContext.Provider>
   );
 };
