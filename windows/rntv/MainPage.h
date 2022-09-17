0e33fed2d0d4bbdb2db9dbc24c906822811d7e6d
dev6699 <kweijack.dev@gmail.com>
2022-09-18 06:45:06 +0800

    feat: react-native-windows

---
+++ windows/rntv/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rntv::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rntv::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
