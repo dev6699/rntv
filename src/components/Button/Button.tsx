91b19dfb0b42d4fb2766478b0a5e2b567c802b20
kweijack <kweijack.dev@gmail.com>
2023-03-07 19:19:57 +0800

    feat: button component with icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
-import { Text, TextStyle, TouchableHighlight, ViewStyle } from 'react-native';
+import { Text, TextStyle, TouchableHighlight, View, ViewStyle } from 'react-native';
 import { theme } from '../../utils';
 
 export const Button: React.FC<{
   text: string;
   onPress: () => void;
+  touchStyle?: ViewStyle
   style?: ViewStyle;
   textStyle?: TextStyle;
-}> = ({ onPress, text, style, textStyle }) => {
+  icon?: JSX.Element
+}> = ({ onPress, text, touchStyle, style, textStyle, icon }) => {
   return (
     <TouchableHighlight
       underlayColor={theme.whiteA(0.2)}
-      style={{
-        padding: 5,
+      onPress={onPress}
+      style={touchStyle}
+    >
+      <View style={{
+        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
+        flex: 1,
+        padding: 5,
         ...style,
       }}
-      onPress={onPress}>
-      <Text
-        style={{
-          padding: 8,
-          fontSize: 18,
-          color: theme.whiteA(),
-          ...textStyle,
-        }}>
-        {text}
-      </Text>
+      >
+        {icon}
+        <Text
+          style={{
+            padding: 8,
+            fontSize: 18,
+            color: theme.whiteA(),
+            ...textStyle,
+          }}>
+          {text}
+        </Text>
+      </View>
     </TouchableHighlight>
   );
 };
