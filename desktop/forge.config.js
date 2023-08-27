38126eda6d1d3045e52125ad0019923337793245
kweijack <kweijack.dev@gmail.com>
2023-08-27 20:22:19 +0800

    feat: electron desktop wrapper for web build

---
+++ desktop/forge.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          maintainer: 'kweijack',
+          homepage: 'https://github.com/dev6699/rntv',
+        },
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
