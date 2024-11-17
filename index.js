aa57e86d3c5efe9f666281c1fb4887af00342f09
kweijack <kweijack.dev@gmail.com>
2024-11-17 13:22:02 +0800

    deps: update react-native 0.76

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 import { AppRegistry, Platform } from 'react-native';
 
 import App from './App';
-import { name as appName } from './app.json';
+import { expo } from './app.json';
 
 if (Platform.OS === 'web') {
-  const expo = require('expo');
-  expo.registerRootComponent(App);
+    const expo = require('expo');
+    expo.registerRootComponent(App);
 } else {
-  AppRegistry.registerComponent(appName, () => App);
+    AppRegistry.registerComponent(expo.name, () => App);
 }
