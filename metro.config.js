1cd56ab6ae5836484daf643a27cc67b0098a75a0
kweijack <kweijack.dev@gmail.com>
2025-06-11 09:14:59 +0800

    deps: update react-native 0.79.3

--- metro.config.js
+++ metro.config.js
@@ -14,7 +14,7 @@ if (process.env.IS_WEB) {
     * Metro configuration
     * https://reactnative.dev/docs/metro
     *
-    * @type {import('metro-config').MetroConfig}
+    * @type {import('@react-native/metro-config').MetroConfig}
     */
     const config = {};
 
